bf25f8805c204fa0a37f2496c1bb7a684cfab826
JYang17 <yangzhe1122@sina.com.cn>
2018-05-20 20:57:35 +0800

    save and restore the model and parameters

---
+++ Chapter 8/8-2 restore model and parameters.py
@@ -0,0 +1,73 @@
+# https://www.bilibili.com/video/av20034351/?p=24
+#based on 4-1
+#https://www.bilibili.com/video/av20034351/?p=10
+#不使用卷积神经网络的MNIST
+import tensorflow as tf 
+from tensorflow.examples.tutorials.mnist import input_data
+#import numpy as np 
+
+#载入数据集
+#one-hot：将标签转化为的某一行，某一位是1，其他位都是0的形式
+#"MNIST_data"创建一个当前程序路径下的文件夹，下载存放MNIST图片数据，也可以用绝对路径
+mnist = input_data.read_data_sets("MNIST_data",one_hot=True)
+
+#每个批次大小
+batch_size = 100
+#计算一共有多少个批次,除完取整
+n_batch = mnist.train.num_examples//batch_size
+
+#placeholder,x对应的是0-9手写图像数据(images),y对应的是实际上的数字是多少(labels)
+#对于[None,784]你可以想，每次输入1行，784列，会输入很多这样的1行
+x = tf.placeholder(tf.float32,[None,784])#任意行，784列
+y = tf.placeholder(tf.float32,[None,10])#任意行，10列（输出10个标签）
+
+#创建一个没有隐藏层的神经网络
+W = tf.Variable(tf.zeros([784,10]))
+#[10,10]不行,tf.zeros([1,10]和tf.zeros([10]是等效的，10列输出每列代表0-9中的一个识别选项
+b = tf.Variable(tf.zeros([1,10]))
+#用softmax函数作为activation function，将计算出0-9每种选项的概率
+#b站网友说：tf.nn.softmax_cross_entropy_with_logits，这个函数会先做softmax，4-1中作者换loss的时候没有吧上面的softmax删除
+prediction = tf.matmul(x,W)+b#tf.nn.softmax(tf.matmul(x,W)+b)
+#我对比试验了两组，prediction = tf.matmul(x,W)+b的精确度稍微高于tf.nn.softmax(tf.matmul(x,W)+b)
+#后面的代码基于4.1做的，也有这个问题
+
+#对数似然代价函数/交叉熵，我对比了一下，比二次代价函数收敛的速度快
+#因为对数似然代价函数后期的调整速度，与误差的大小成正比，所以收敛速度比二次代价函数快
+# loss = tf.reduce_mean(tf.square(y-prediction))
+# loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y,logits=prediction))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y,logits=prediction))
+
+#梯度下降法优化器
+train = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
+
+init = tf.global_variables_initializer()
+
+#tf.argmax(y,1)代表概率最大的对于那个位置，
+#如果预测的和实际的概率最大位置一样，说明所识别的数字是一致的，就认为识别数字正确
+correction_prediction = tf.equal(tf.argmax(y,1),tf.argmax(prediction,1))
+
+#求准确率
+#tf.cast(prediction,tf.float32)将bool转化为float就是准确率
+#这个平均值应该是这波输入图片全集（不论训练集或测试集）的所以训练批次的平均值
+accuracy = tf.reduce_mean(tf.cast(correction_prediction,tf.float32))
+
+#新加的
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for step in range(21):#总共数据反复训练21次
+        for batch in range(n_batch):#注意要加range，每批数据数据集分成多个批次
+            batch_xs,batch_ys = mnist.train.next_batch(batch_size)
+            sess.run(train,feed_dict={x:batch_xs,y:batch_ys})
+        
+        #用测试集，进行测试
+        acc = sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels})
+        print("Step:"+str(step)+" Accuracy:"+str(acc))
+
+        #加载之前保存的model和训练参数
+        saver.restore(sess,"net\mynet.ckpt")
+        restore_acc = sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels})
+        print("Step:"+str(step)+" restored model accuracy:"+str(restore_acc))
+    
+
